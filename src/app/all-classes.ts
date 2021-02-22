3092b90272cf6e084efa193d607aef94cd584b9a
Patrik Radics <54003031+Codethier@users.noreply.github.com>
2021-02-22 18:40:00 +0100

    classes are done

--- src/app/all-classes.ts
+++ src/app/all-classes.ts
@@ -51,14 +51,14 @@ export class Address {
 
 export class Order {
   private id: number;
-  private custromerID: number;
+  private customerID: number;
   private productID: number;
   private amount: number;
   private status: 'new' | 'shipped' | 'paid';
 
-  constructor(id: number, custromerID: number, productID: number, amount: number, status: 'new' | 'shipped' | 'paid') {
+  constructor(id: number, customerID: number, productID: number, amount: number, status: 'new' | 'shipped' | 'paid') {
     this.id = id;
-    this.custromerID = custromerID;
+    this.customerID = customerID;
     this.productID = productID;
     this.amount = amount;
     this.status = status;
