246a41a0a416316ffac9448129a5bfb5499548f5
Joe <zsklari11@gmail.com>
2021-02-23 12:22:52 +0100

    order.service

---
+++ src/app/service/order.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Order } from 'src/app/models/Order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor() { }
+}
