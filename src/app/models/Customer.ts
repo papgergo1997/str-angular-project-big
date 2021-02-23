0f94533725a1581ea1142942fb1647e9afc12c3a
papgergo1997 <pap.gergo.1997@gmail.com>
2021-02-23 13:59:54 +0100

    customer.ts javítva

--- src/app/models/Customer.ts
+++ src/app/models/Customer.ts
@@ -1,16 +1,11 @@
 import { Address } from './Address';
 
 export class Customer {
-<<<<<<< HEAD
+
   id: number = 0;
   firstName: string = '';
   lastName: string = '';
   email: string = '';
   address: Address = new Address();
   active: boolean = false;
-=======
-  constructor(private id: number, private firstName: string, private lastName: string, private email: string,
-              private adress: Address, private active: boolean) {
-  }
->>>>>>> origin/dev
 }
