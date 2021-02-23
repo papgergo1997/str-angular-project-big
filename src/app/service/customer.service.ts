1837b704e712356a7baf627d1246dd16dc4e0478
papgergo1997 <pap.gergo.1997@gmail.com>
2021-02-23 13:54:22 +0100

    customer.service getall() létrehozva

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Customer } from '../models/Customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  customerList$: Observable<Customer[]>;
+  apiUrl: string = 'http://localhost:3000/customers';
+
+  constructor(private http: HttpClient) {
+    this.customerList$ = this.getAll();
+  }
+
+  getAll(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.apiUrl);
+  }
+
+
 }
