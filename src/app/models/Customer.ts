d19217db46ace97cb2cd903bb847c56e1171cbe5
Patrik Radics <54003031+Codethier@users.noreply.github.com>
2021-02-23 17:28:29 +0100

    stuff is public

--- src/app/models/Customer.ts
+++ src/app/models/Customer.ts
@@ -2,6 +2,6 @@ import {Address} from './Address';
 
 export class Customer {
   constructor(public  id: number, public  firstName: string, public  lastName: string, public  email: string,
-              public  adress: Address, public  active: boolean) {
+              public  address: Address, public  active: boolean) {
   }
 }
