aa459f7e582f0baeb51eb4f50abab4e0bcce057c
Kovács Dorottya <kovacsdorika@gmail.com>
2021-02-24 17:36:14 +0100

    Edit bill basics.

--- src/app/edit/edit-bill/edit-bill.component.ts
+++ src/app/edit/edit-bill/edit-bill.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { Bill } from 'src/app/models/Bill';
 import { BillService } from 'src/app/service/bill.service';
+import { Bill } from 'src/app/models/Bill';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-bill',
@@ -12,28 +13,29 @@ import { BillService } from 'src/app/service/bill.service';
 })
 export class EditBillComponent implements OnInit {
 
+  updating: boolean = false;
   bill$: Observable<Bill> = this.activatedRoute.params.pipe(
-    switchMap(params => this.billService.get(params.id))
+    switchMap( params => this.billService.get(params.id) )
   );
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private billService: BillService,
-    private router: Router,) { }
+    private router: Router,
+  ) { }
 
-  ngOnInit(
+  ngOnInit(): void { }
 
-  ): void {
-  }
 
-  onUpdate(bill: Bill): void {
-    if (bill.id === 0) {
+  onUpdate(form: NgForm, bill: Bill): void {
+    this.updating = true;
+    if(bill.id === 0) {
       this.billService.create(bill);
       this.router.navigate(['bills']);
-    } else {
-      this.billService.update(bill).subscribe(
-        () => this.router.navigate(['bills'])
-      );
     }
+    this.billService.update(bill).subscribe(
+      () => this.router.navigate(['bills'])
+    );
   }
 }
