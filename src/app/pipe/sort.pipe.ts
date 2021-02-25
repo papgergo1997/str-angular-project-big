03725a31c6b4382dda9054a793ac3abd02d866f9
papgergo1997 <pap.gergo.1997@gmail.com>
2021-02-25 10:46:00 +0100

    sort pipe content created

--- src/app/pipe/sort.pipe.ts
+++ src/app/pipe/sort.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any[], key: string): any[] {
+
+    if (!Array.isArray(value) || !key)
+      return value;
+
+    return value.sort(function (a, b) {
+      if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+        return a[key] - b[key];
+      } else {
+        return a[key].toString().toLowerCase().localeCompare(b[key].toString().toLowerCase());
+      }
+    })
   }
 
 }
