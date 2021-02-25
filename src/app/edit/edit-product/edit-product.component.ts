5548683bfcc38445d01923d2d68f4273eddfb51b
pinjanos <pin.janos@gmail.com>
2021-02-25 10:35:43 +0100

    product and categories json is already English

--- src/app/edit/edit-product/edit-product.component.ts
+++ src/app/edit/edit-product/edit-product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Product } from 'src/app/models/Product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  product$: Observable<Product> = this.activatedRoute.params.pipe(
+    switchMap( params => this.productService.get(params.id))
+  );
+
+  product: Product = new Product();
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onUpdate(form: NgForm, product: Product): void {
+    if (product.id !== 0) {
+      this.productService.update(product);
+      this.router.navigate(['products']);
+    }
+    this.productService.create(product);
+    this.router.navigate(['products']);
   }
 
 }
